9b4a57afbf30c52e7b98fa6e408c5c7ddc0bbac7
dodg1444 <58045542+dodg1444@users.noreply.github.com>
2019-12-20 18:28:02 +0300

    Add files via upload

--- spelling.py
+++ spelling.py
@@ -4,20 +4,12 @@ import re
 
 
 def transcr(your_word):
-    url = 'https://www.wordreference.com/definition/'+str(your_word)
+    url = 'https://www.macmillandictionary.com/dictionary/british/'+str(your_word)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, features="html.parser")
-    s1 = str(soup).splitlines()
-    match = 'placeholder'
-    for elem in s1:
-        if 'pronWR' in elem:
-            match = re.search('\[(.*?)\]', elem).group(0)[1:-1]
-            if match != 'placeholder':
-                break
-    if match == 'placeholder':
-        raise Exception('no such word')
-    else:
-        return match
+    soup_by_tag = soup.find("span", {"class":"PRON show_less"})
+    match = re.findall('\>(.*?)\<', str(soup_by_tag))
+    return match[2]
 
 
 print('What?')
@@ -28,9 +20,12 @@ while True:
     else:
         lenght = 2*len(word)+13
     separator = str()
-    for i in range(0,lenght):
+    for i in range(0, lenght):
         separator = separator+str('-')
-    trancscripted = transcr(word).replace('<sup>', '').replace('</sup>', '').replace('<i>', '').replace('</i>', '').replace('<small>', '').replace('</small>', '')
-    print(separator)
-    print('|', word, ' --> ', trancscripted, ' ', '|')
-    print(separator)
+    try:
+        trancscripted = transcr(word)
+        print(separator)
+        print('|', word, ' --> ', trancscripted, ' ', '|')
+        print(separator)
+    except IndexError as error:
+        print("Dude, that's messed up")
