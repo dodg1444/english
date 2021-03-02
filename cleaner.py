858b610db00a17e8167a00596130feb8871d016e
dodg1444 <58045542+dodg1444@users.noreply.github.com>
2021-03-02 10:54:54 +0300

    Delete cleaner.py

--- cleaner.py
+++
@@ -1,7 +0,0 @@
-dirty = open('dirty.txt', 'r', encoding='utf8')
-clean = open('clean.txt', 'a', encoding='utf8')
-lines = dirty.readlines()
-for i in range(len(lines)):
-    if '-' in lines[i]:
-        clean.writelines(lines[i])
-clean.close()
